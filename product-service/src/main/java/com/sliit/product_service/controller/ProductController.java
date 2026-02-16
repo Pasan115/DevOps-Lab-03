7391d2fd5fa09713cfc5a7f3bc02bef0a71f788d
Amarasinghe W.A.P.M <pasanamarasinghe2003@gmail.com>
2026-02-16 09:59:02 +0530

    Add files via upload

---
+++ product-service/src/main/java/com/sliit/product_service/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.sliit.product_service.controller;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import com.sliit.product_service.entity.Product;
+import com.sliit.product_service.repository.ProductRepository;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository repository;
+
+    public ProductController(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return repository.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product getById(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
